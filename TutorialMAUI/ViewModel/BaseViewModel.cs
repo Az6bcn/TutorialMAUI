7ef6e5742915eb49d6f05f881f8c60399011f222
Azeez <azeez.odumosu@victorianplumbing.co.uk>
2022-09-19 08:45:35 +0100

    Changed written MVVM to use the communityToolkit.Mvvm

--- TutorialMAUI/ViewModel/BaseViewModel.cs
+++ TutorialMAUI/ViewModel/BaseViewModel.cs
@@ -1,55 +1,25 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿
+
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace TutorialMAUI.ViewModel;
 
-public class BaseViewModel: INotifyPropertyChanged
+//[INotifyPropertyChanged]
+public partial class BaseViewModel : ObservableObject
 {
-    private bool _isBusy;
-    private string _title;
-
-    public bool IsBusy
+    public BaseViewModel()
     {
-        get => _isBusy;
-        set
-        {
-            if(_isBusy == value)
-                return;
-
-            _isBusy = value;
-            OnPropertyChanged(nameof(IsBusy));
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
+        
     }
 
-    public string Title
-    {
-        get => _title;
-        set
-        {
-            if (_title == value)
-                return;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool IsBusy;
 
-            _title = value;
-            OnPropertyChanged(nameof(Title));
-        }
-    }
+    [ObservableProperty]
+    string title;
 
-    public bool IsNotBusy => !IsBusy;
-    
-    public event PropertyChangedEventHandler PropertyChanged;
+    public bool IsNotBusy;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-    {
-        // If the event is not null i.e anyone subscribed to the event => then invoke it
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
 }
